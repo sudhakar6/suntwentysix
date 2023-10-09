f94040773430f64238d560ec9ea46ed9a59f19b8
Sudhakar <9988005+sudhakar6@users.noreply.github.com>
2023-10-09 16:33:25 +0200

    1.THIS_IS_FOR_Alejandro_ON_f1_from_master

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    THIS_IS_FOR_Alejandro_ON_f1_from_master 
 };
