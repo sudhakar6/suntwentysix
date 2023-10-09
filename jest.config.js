d7d31e9adcd1dfbf07718de0ef525e45b092e870
Sudhakar <9988005+sudhakar6@users.noreply.github.com>
2023-10-09 16:36:06 +0200

    2.THIS_IS_FOR_Barish_ON_for_QA_from_f1_from_master

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     ...jestConfig,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
     THIS_IS_FOR_Alejandro_ON_f1_from_master 
+    THIS_IS_FOR_Barish_ON_for_QA_from_f1_from_master
 };
